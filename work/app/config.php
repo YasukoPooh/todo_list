0796ab89ecb4cf38c22eb7eefca38416de8138f2
YasukoPooh <saiyasu.tksf@gmail.com>
2021-07-28 05:26:05 +0900

    todo

--- work/app/config.php
+++ work/app/config.php
@@ -5,6 +5,7 @@ session_start();
 define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 spl_autoload_register(function ($class) {
   $prefix = 'MyApp\\';
